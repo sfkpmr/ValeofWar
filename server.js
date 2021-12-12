93285079009ac7bc90bce2c54a0a87a7180a7ce0
sfkpmr <sfkpmr@users.noreply.github.com>
2021-12-12 22:49:28 +0100

    Basic functionality

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const app = express();
+const { MongoClient } = require('mongodb');
 require("dotenv").config();
 const { auth, requiresAuth } = require('express-openid-connect');
 const ejs = require('ejs');
 app.set('view engine', 'ejs');
+
+const uri = "mongodb+srv://server:zjzJoTWpk322w2eJ@cluster0.rn9ur.mongodb.net/gamedb?retryWrites=true&w=majority"
+const client = new MongoClient(uri);
+
+main();
+
+async function main() {
+
+    try {
+        console.log("aaaaa");
+        await client.connect();
+        console.log("bbbbb");
+        //await findUser(client, "johanna");
+    } catch (e) {
+        console.error(e);
+    } //finally {
+    //await client.close();
+    //}
+
+
+    //main().catch(console.error);
+}
 
 app.use(
     auth({
@@ -25,13 +48,17 @@ app.get("/", (req, res) => {
 });
 
 app.get("/profile", requiresAuth(), (req, res) => {
+
     res.send(JSON.stringify(req.oidc.user));
 
     //Test user: test@test.com, iosudht0pi346h9-
 });
 
-app.get("/profile/:username", requiresAuth(), (req, res) => {
-    res.send(req.params.username);
+app.get("/profile/:username", requiresAuth(), async (req, res) => {
+
+    const apa = await findUser(client, req.params.username);
+
+    res.send(apa);
 });
 
 app.get("/base", requiresAuth(), (req, res) => {
@@ -43,3 +70,18 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+async function findUser(client, username) {
+    //TODO https://docs.mongodb.com/manual/reference/method/db.collection.findOne/#specify-the-fields-to-return
+    const result = await client.db("gamedb").collection("players").findOne({ username: username });
+
+    if (result) {
+        console.log(`Found a listing ${username}`);
+        console.log(result);
+    } else {
+        console.log("No listing");
+        return "None";
+    }
+
+    return result;
+}
